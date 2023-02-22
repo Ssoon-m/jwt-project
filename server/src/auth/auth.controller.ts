b4a22693f5386c54341657805272ec776d36bec5
Ssoon-m <tkksm2@gmail.com>
2023-02-22 12:16:47 +0900

    fix: token생성시 user foreignkey 생성 안 되는 이슈

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Get,
@@ -9,7 +10,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { LocalAuthGuard } from './local-auth.guard';
-import { AuthService } from './auth.service';
+import { AuthService, ITokenResponse } from './auth.service';
 import { Request, Response } from 'express';
 
 @Controller('auth')
@@ -17,24 +18,39 @@ export class AuthController {
   constructor(private authService: AuthService) {}
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Req() req: any, @Res({ passthrough: true }) res: Response) {
-    const { accessToken, refreshToken } = await this.authService.generateTokens(
-      {
-        userId: req.user.id,
-        username: req.user.username,
-      },
-    );
-    res.cookie('access_token', accessToken, {
+  async login(
+    @Req() req: { user: ITokenResponse },
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    res.cookie('access_token', req.user.accessToken, {
       httpOnly: true,
       expires: new Date(Date.now() + 1000 * 30),
       path: '/',
     });
-    res.cookie('refresh_token', refreshToken, {
+    res.cookie('refresh_token', req.user.refreshToken, {
       httpOnly: true,
       expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
       path: '/',
     });
-    return { accessToken, refreshToken };
+    return {
+      accessToken: req.user.accessToken,
+      refreshToken: req.user.refreshToken,
+    };
+  }
+
+  @Post('refresh')
+  refresh(
+    @Req() req: Request,
+    @Body() { refresh }: { refresh?: string },
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const refreshToken = refresh ?? req.cookies['refresh_token'] ?? '';
+    if (!refreshToken) {
+      throw new BadRequestException('empty refresh token');
+    }
+
+    this.authService.verifyRefreshToken(refreshToken);
+    return 'sdf';
   }
 
   @Get('me')
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -3,10 +3,16 @@ import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserInfoDto } from 'src/users/dto/user.dto';
+import { User } from 'src/users/entity/user.entity';
 import { Repository } from 'typeorm';
 import { UsersService } from '../users/users.service';
 import { RefreshTokenDTO, AccessTokenDTO } from './dto/token.dto';
 import { Token } from './entity/token.entity';
+
+export interface ITokenResponse {
+  accessToken: string;
+  refreshToken: string;
+}
 
 @Injectable()
 export class AuthService {
@@ -18,11 +24,14 @@ export class AuthService {
     private tokenRepository: Repository<Token>,
   ) {}
 
-  async validateUser(username: string, password: string): Promise<any | null> {
+  async validateUser(
+    username: string,
+    password: string,
+  ): Promise<ITokenResponse | null> {
     const user = await this.usersService.findOne(username);
     if (user && user.password === password) {
-      const { password, ...result } = user;
-      return result;
+      const { accessToken, refreshToken } = await this.generateTokens(user);
+      return { accessToken, refreshToken };
     }
     return null;
   }
@@ -43,13 +52,13 @@ export class AuthService {
     return token;
   }
 
-  async generateTokens(user: UserInfoDto, tokenItem?: Token): Promise<any> {
+  async generateTokens(user: User, tokenItem?: Token): Promise<ITokenResponse> {
     const token = tokenItem ?? (await this.tokenRepository.save({ user }));
     const tokenId = token.id;
 
     const [accessToken, refreshToken] = await Promise.all([
       this.signAccessToken({
-        userId: user.userId,
+        userId: user.id,
         username: user.username,
         tokenId,
       }),
@@ -68,6 +77,30 @@ export class AuthService {
         secret: this.configService.get('JWT_ACCESS_TOKEN_SECRET'),
       });
       return verify;
+    } catch (e) {
+      switch (e.name) {
+        case 'TokenExpiredError':
+          throw new UnauthorizedException('token expired error');
+        case 'JsonWebTokenError':
+          throw new UnauthorizedException('json web token error');
+      }
+      throw e;
+    }
+  }
+
+  async verifyRefreshToken(refreshToken: string) {
+    try {
+      const { tokenId, rotationCounter } =
+        this.jwtService.verify<RefreshTokenDTO>(refreshToken, {
+          secret: this.configService.get('JWT_REFRESH_TOKEN_SECRET'),
+        });
+      const tokenItem = await this.tokenRepository.find({
+        where: {
+          id: tokenId,
+        },
+        relations: ['user'],
+      });
+      console.log('tokenItem', tokenItem);
     } catch (e) {
       switch (e.name) {
         case 'TokenExpiredError':
